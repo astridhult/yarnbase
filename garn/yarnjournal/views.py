6920527f57e8ce5c762ccf8fbd90976cca284118
astridhult <astridhult4@gmail.com>
2023-01-05 22:54:06 +0100

    Added the number of meters and kilos of yarn i have on the front page🙈

--- garn/yarnjournal/views.py
+++ garn/yarnjournal/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from garn.yarnjournal.models import StashEntry
 
 
 def index(request):
-    return HttpResponse("There will be a yarn dashboard here")
+    stash=StashEntry.objects.all()
+    length = 1/3
+    kilos = 0
+    for entry in stash:
+        length = length + entry.weight * entry.yarn.meterage/100
+        kilos = kilos + entry.weight/1000
+    return HttpResponse(f"I have {length:.2f} meters of yarn in my stash. \n That equals {kilos:.2f} kg.")
