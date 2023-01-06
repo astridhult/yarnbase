01997d8c4c69a4f88217fc1aab03b5d1d7bb34d0
astridhult <astridhult4@gmail.com>
2023-01-06 23:04:37 +0100

    Added status to the display of projects, so that is visible in the list on the admin site.

--- garn/yarnjournal/models.py
+++ garn/yarnjournal/models.py
@@ -32,10 +32,10 @@ class Project(models.Model):
     notes = models.TextField(null=True, blank=True)
     recipient = models.CharField(max_length=200, null=True, blank=True)
     size = models.CharField(max_length=200, null=True, blank=True)
-    status_options = [("planned","planned"), ("in progress", "in progress"), ("finished", "finished"), ("frogged", "frogged")]
+    status_options = [("planned","Planned"), ("in progress", "In progress"), ("finished", "Finished"), ("frogged", "Frogged")]
     status = models.CharField(max_length=200, choices=status_options)
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.get_status_display()}"
 
 
 class StashEntry(models.Model):
